0103eb0ed63ab537f65f9f4545cd1c4b30a4d72a
Max Souders <soudersmax@gmail.com>
2025-09-08 22:32:42 +0000

    word cloud edits and additions

--- intentions_dict_import_example.py
+++
@@ -1,4 +0,0 @@
-import json
-
-with open('/workspaces/hh-conf/intentions_dict.json', 'r') as f:
-    intentions_dict = json.load(f)
--- text_analysis.py
+++ text_analysis.py
@@ -6,35 +6,41 @@ import matplotlib.pyplot as plt
 import os
 import json
 
-with open('intentions_dict.json', 'r') as f:
+# Load all dicts from correct file paths
+with open('/workspaces/hh-conf/Data/intentions_dict.json', 'r') as f:
     intentions_dict = json.load(f)
+with open('/workspaces/hh-conf/Data/impact_dict.json', 'r') as f:
+    impact_dict = json.load(f)
 
 # If not already downloaded:
-nltk.download('popular')
-nltk.download('punkt_tab')
+nltk.download('punkt')
 nltk.download('stopwords')
 
-# Flatten all custom intentions into a single list
-all_intentions = [item for sublist in intentions_dict.values() for item in sublist]
-
-# Tokenize and clean
-tokens = [word.lower() for item in all_intentions for word in word_tokenize(item, language='english')]
+custom_exclude = {
+    'hollyhock', 'spend', 'program', 'week', 'mom', 'teenage', 'cosmo', 'part',
+    'drinking', 'within', 'tourists', 'deck', 'happened', 'gained', 'fascism', 'children',
+    'flora', 'merlin', 'alcohol', 'atum', 'bullet', 'dogs', 'babatunde', 'seems', 'carried',
+    'programs', 'coming', 'medium', 'specifically', 'olatunji', 'barking', 'dig', 'campus',
+    'last', 'big'
+}
 stop_words = set(stopwords.words('english'))
-custom_exclude = {'hollyhock', 'spend', 'program', 'week', 'mom', 'teenage', 'cosmo', 'part'}
-filtered_tokens = [w for w in tokens if w.isalpha() and w not in stop_words and w not in custom_exclude]
 
-# Frequency distribution
-fdist = nltk.FreqDist(filtered_tokens)
-print('Most common words:', fdist.most_common(10))
+def analyze_and_wordcloud(data_dict, output_filename):
+    all_text = [item for sublist in data_dict.values() for item in sublist]
+    tokens = [word.lower() for item in all_text for word in word_tokenize(item, language='english')]
+    filtered_tokens = [w for w in tokens if w.isalpha() and w not in stop_words and w not in custom_exclude]
+    fdist = nltk.FreqDist(filtered_tokens)
+    print(f'Most common words in {output_filename}:', fdist.most_common(10))
+    text = ' '.join(filtered_tokens)
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+    plt.figure(figsize=(10,5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.title(f'Word Cloud: {output_filename}')
+    plt.show()
+    wordcloud.to_file(output_filename)
+    print(f'Word cloud saved to {output_filename}')
 
-# Word cloud
-text = ' '.join(filtered_tokens)
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
-plt.figure(figsize=(10,5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title('Custom Intentions Word Cloud')
-plt.show()
-output_path = 'intentions_wordcloud.png'
-wordcloud.to_file(output_path)
-print(f'Word cloud saved to {output_path}')
+# Run analysis for each dict
+analyze_and_wordcloud(intentions_dict, 'intentions_wordcloud.png')
+analyze_and_wordcloud(impact_dict, 'impact_wordcloud.png')
